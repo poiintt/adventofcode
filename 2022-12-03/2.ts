9123f1db9d5445da73d915e2108fd55808e2b36f
poiintt <abendmo@gmail.com>
2022-12-04 02:27:14 +0100

    feat: add 2022-12-03/2 solution

---
+++ 2022-12-03/2.ts
@@ -0,0 +1,28 @@
+import { readFile } from "fs/promises";
+
+const input = (await readFile("./input1.txt")).toString();
+const backpacks = input.split("\n");
+
+let priorities = 0;
+for (let i = 0; i < backpacks.length / 3; i++) {
+  const groupBackpacks = backpacks.slice(i * 3, i * 3 + 3);
+
+  const firstBackpackLetters = groupBackpacks[0].split("");
+  const otherBackpacks = groupBackpacks.slice(1);
+
+  const badges = firstBackpackLetters.filter((letter) =>
+    otherBackpacks.every((backpack) => backpack.split("").includes(letter))
+  );
+  console.log({ badges });
+  const badge = badges[0];
+
+  if (badge.toLowerCase() === badge) {
+    const prio = badge.charCodeAt(0) - 96;
+    priorities += prio;
+  } else {
+    const prio = badge.charCodeAt(0) - 38;
+    priorities += prio;
+  }
+}
+
+console.log({ priorities });
