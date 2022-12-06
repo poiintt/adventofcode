f1e19429e3b3ef3df2eafb5733c3645f46f5c62d
poiintt <abendmo@gmail.com>
2022-12-06 21:15:42 +0100

    feat: add 2022/02 solution

---
+++ 2022/02/1.ts
@@ -0,0 +1,47 @@
+import { readFile } from "fs/promises";
+
+function getResult(input: string) {
+  const strategies = input.toString().split("\n");
+
+  type Hands = "Rock" | "Paper" | "Scissors";
+  const hands: Record<
+    Hands,
+    [opponent: string, me: string, score: number, beats: Hands]
+  > = {
+    Rock: ["A", "X", 1, "Scissors"],
+    Paper: ["B", "Y", 2, "Rock"],
+    Scissors: ["C", "Z", 3, "Paper"],
+  };
+
+  return strategies.reduce((score, curr) => {
+    const [opponentEncoded, meEncoded] = curr.split(" ");
+
+    const opponent = Object.entries(hands).find(
+      (e) => e[1][0] === opponentEncoded
+    )?.[0];
+    const me = Object.entries(hands).find((e) => e[1][1] === meEncoded)?.[0];
+
+    if (opponent && me) {
+      if (opponent === me) {
+        score += 3;
+      } else if (hands[me][3] === opponent) {
+        score += 6;
+      }
+      score += hands[me][2];
+    }
+    return score;
+  }, 0);
+}
+
+const example = await readFile("./example.txt", { encoding: "utf8" });
+const puzzle = await readFile("./puzzle.txt", { encoding: "utf8" });
+
+console.time("example");
+const exampleResult = getResult(example);
+console.timeEnd("example");
+
+console.time("puzzle");
+const puzzleResult = getResult(puzzle);
+console.timeEnd("puzzle");
+
+console.log({ exampleResult, puzzleResult });
