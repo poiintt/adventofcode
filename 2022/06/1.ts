acbff573f9cf6c9f98a334fbc52481aa11a1f1ab
poiintt <abendmo@gmail.com>
2022-12-06 19:30:40 +0100

    feat: add 2022/06 solution

---
+++ 2022/06/1.ts
@@ -0,0 +1,30 @@
+import { readFile } from "fs/promises";
+
+const input = await readFile("./puzzle.txt");
+
+const datastreams = input.toString().split("\n");
+
+for (const datastream of datastreams) {
+  const lastFour: string[] = [];
+  for (let i = 0; i < datastream.length; i++) {
+    const char = datastream[i];
+    lastFour.push(char);
+
+    if (lastFour.length > 4) {
+      lastFour.shift();
+
+      const res = lastFour.every((letter, i, arr) => {
+        for (const [i2, x] of arr.entries()) {
+          if (i !== i2 && x === letter) {
+            return false;
+          }
+        }
+        return true;
+      });
+      if (res) {
+        console.log(i + 1);
+        break;
+      }
+    }
+  }
+}
