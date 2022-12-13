3d39750d350100f3f005bea361739e2480dda3a6
poiintt <abendmo@gmail.com>
2022-12-13 23:17:36 +0100

    feat: add 2022/13 1 first working solution
    this was doable

---
+++ 2022/13/1.ts
@@ -0,0 +1,70 @@
+import { readFile } from "fs/promises";
+
+function getResult(input: string) {
+  const packetPairs = input
+    .split("\n\n")
+    .map((packetPair) => packetPair.split("\n"));
+
+  const rightOrderIndices: number[] = [];
+
+  type NumberOrArray = number | number[] | NumberOrArray[];
+  function recurse(
+    left: NumberOrArray,
+    right: NumberOrArray
+  ): boolean | undefined {
+    // console.log(JSON.stringify(left), "vs", JSON.stringify(right));
+    if (Array.isArray(left) && Array.isArray(right)) {
+      const length = left.length >= right.length ? left.length : right.length;
+      for (let i = 0; i < length; i++) {
+        if (left[i] != null && right[i] != null) {
+          const r = recurse(left[i], right[i]);
+          if (r != null) return r;
+        } else if (left[i] == null && right[i] != null) {
+          return true;
+        } else if (left[i] != null && right[i] == null) {
+          return false;
+        }
+      }
+    } else if (Array.isArray(left) && !Array.isArray(right)) {
+      const r = recurse(left, [right]);
+      if (r != null) return r;
+    } else if (!Array.isArray(left) && Array.isArray(right)) {
+      const r = recurse([left], right);
+      if (r != null) return r;
+    } else {
+      if (left < right) {
+        return true;
+      } else if (left > right) {
+        return false;
+      }
+    }
+  }
+
+  for (const [pairIndex, [leftText, rightText]] of packetPairs.entries()) {
+    // this is cursed
+    const left = eval(leftText);
+    const right = eval(rightText);
+
+    const result = recurse(left, right);
+    if (result === true) {
+      rightOrderIndices.push(pairIndex + 1);
+    }
+  }
+  const sum = rightOrderIndices.reduce((acc, curr) => acc + curr, 0);
+
+  return sum;
+}
+
+const example = await readFile("./example.txt", { encoding: "utf8" });
+const puzzle = await readFile("./puzzle.txt", { encoding: "utf8" });
+
+console.time("example");
+const exampleResult = getResult(example);
+console.timeEnd("example");
+
+console.time("puzzle");
+const puzzleResult = getResult(puzzle);
+console.timeEnd("puzzle");
+
+console.log({ exampleResult, puzzleResult });
+// { exampleResult: 13, puzzleResult: 5506 }
