b2ba3ee37bad1aa5662d75366c3d989bd1912c58
poiintt <abendmo@gmail.com>
2024-12-03 22:18:04 +0100

    refactor(2022/14): part 1
    - update tsconfig and package.json

--- 2022/14/1.ts
+++ 2022/14/1.ts
@@ -19,7 +19,6 @@ function getResult(input: string) {
       return { x, y } as Point;
     })
   );
-  console.log({ xMin, xMax, yMin, yMax });
   const rockPoints: Point[] = [];
 
   for (const rockPath of parsed) {
@@ -33,34 +32,32 @@ function getResult(input: string) {
         break;
       }
 
-      const currX = rock.x;
+      const { x, y } = rock;
       const nextX = rockPath[i + 1].x;
-      const currY = rock.y;
       const nextY = rockPath[i + 1].y;
 
       let dx = 0;
       let dy = 0;
-      if (nextX - currX > 0) {
+      if (nextX - x > 0) {
         dx = +1;
-      } else if (nextX - currX < 0) {
+      } else if (nextX - x < 0) {
         dx = -1;
       }
-      if (nextY - currY > 0) {
+      if (nextY - y > 0) {
         dy = +1;
-      } else if (nextY - currY < 0) {
+      } else if (nextY - y < 0) {
         dy = -1;
       }
 
-      if (currX === nextX && currY === nextY) {
+      if (x === nextX && y === nextY) {
         i++;
       }
       rock = {
-        x: currX + dx,
-        y: currY + dy,
+        x: x + dx,
+        y: y + dy,
       };
     }
   }
-  console.log({ l: rockPoints.length });
 
   type Point = { x: number; y: number };
 
@@ -78,52 +75,24 @@ function getResult(input: string) {
         grid[y].push(".");
       }
     }
-    console.log(grid[y].join(""));
-  }
-  // console.log(grid);
-
-  function print(point: Point) {
-    console.log();
-    for (let y = 0; y < grid.length; y++) {
-      let line = "";
-      for (let x = 0; x < grid[0].length; x++) {
-        if (x === point.x - xMin && y === point.y) {
-          line += "@";
-        } else {
-          line += grid[y][x];
-        }
-      }
-      console.log(line);
-    }
   }
 
   let restingSandCount = 0;
-  let count = 0;
-  while (true) {
-    count++;
-    // console.log(count);
+  outer: while (true) {
     let sandPoint = { x: sandPos.x, y: sandPos.y };
-    let count2 = 0;
-    let fullBreak = false;
 
     while (true) {
-      count2++;
-      // console.log({ count2 });
       const x = sandPoint.x;
       const xMinusOffset = sandPoint.x - xMin;
       const y = sandPoint.y;
 
       if (grid[y + 1] == null) {
-        // console.log(y + 1);
-        // print(sandPoint);
-        fullBreak = true;
-        break;
+        break outer;
       }
 
       const below = grid[y + 1][xMinusOffset];
       if (below == null) {
-        fullBreak = true;
-        break;
+        break outer;
       } else if (below === ".") {
         sandPoint = { x: x, y: y + 1 };
         continue;
@@ -131,8 +100,7 @@ function getResult(input: string) {
 
       const left = grid[y + 1][xMinusOffset - 1];
       if (left == null) {
-        fullBreak = true;
-        break;
+        break outer;
       } else if (left === ".") {
         sandPoint = { x: x - 1, y: y + 1 };
         continue;
@@ -140,8 +108,7 @@ function getResult(input: string) {
 
       const right = grid[y + 1][xMinusOffset + 1];
       if (right == null) {
-        fullBreak = true;
-        break;
+        break outer;
       } else if (right === ".") {
         sandPoint = { x: x + 1, y: y + 1 };
         continue;
@@ -149,20 +116,13 @@ function getResult(input: string) {
 
       grid[y][xMinusOffset] = "o";
       restingSandCount++;
-      // console.log(grid.map((l) => l.join("")).join("\n"));
       break;
     }
-    console.log({ restingSandCount });
-    // print(sandPoint);
 
-    if (fullBreak || sandPoint.y > yMax) {
-      // console.log(sandPoint.x - xMin);
-      print(sandPoint);
+    if (sandPoint.y > yMax) {
       break;
     }
   }
-  // console.log(grid.map((l) => l.join("")).join("\n"));
-  console.log({ yMax });
   return restingSandCount;
 }
 
